5006891bb40e09a798e1c5b51f9928d583a35492
root <r05246009@ntu.edu.tw>
2018-06-09 22:27:56 +0800

    size change

--- src/user_program/master.c
+++ src/user_program/master.c
@@ -98,7 +98,7 @@ int main (int argc, char* argv[])
 
         gettimeofday(&end, NULL);
 	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec);
-	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size);
 
 	return 0;
 }
